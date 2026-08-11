c2299e5fb821bbb802d8d4e32e4f26cc8d6a81c7
Nama Anda <fahriabdurahmansoleh@gmail.com>
2026-08-11 22:38:11 +0700

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">Money Tracker</h1>
+      <h1 className="text-2xl font-bold">Money Tracker | In Development</h1>
     </main>
   );
 }
